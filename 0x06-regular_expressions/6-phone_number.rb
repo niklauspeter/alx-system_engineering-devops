f220d85c83bd5589cdeb57b0831b9bb51970c001
klaus <oriokiklaus@gmail.com>
2023-03-01 02:39:30 +0300

    change so first element is always a digit

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 #regular expression matches a 1-digit phone number
-puts ARGV[0].scan(/\d{3}[ -]?\d{3}[ -]?\d{4}/).join
+#puts ARGV[0].scan(/\d{3}[ -]?\d{3}[ -]?\d{4}/).join
+puts ARGV[0].scan(/^\d{10,10}$/).join
