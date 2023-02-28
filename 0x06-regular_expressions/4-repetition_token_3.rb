df87258f143c9d0f4c2a1aad917d1bd51da2f625
klaus <oriokiklaus@gmail.com>
2023-03-01 02:20:04 +0300

    make executable

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-# Prints all the words containing letters of hbtn with t being optional
-# ./1-repetition_token_0.rb
-puts ARGV[0].scan(/hbt*n).join
+# Prints all the characters that match the pattern hbt*n
+puts ARGV[0].scan(/hbt*n/).join
