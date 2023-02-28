563b9d52016f837ad05d00fad88f3d1707bd4d57
klaus <oriokiklaus@gmail.com>
2023-03-01 01:59:21 +0300

    change to max 5 t's

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# Prints all the words that start with h, end with n, and have repeted t's in between.
+# Prints all the words that start with h, end with n, and has one to five t's in between.
 # ./1-repetition_token_0.rb
-puts ARGV[0].scan(/hbt+n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
