6b046979240fb4e631386d6426980606c84f40db
klaus <oriokiklaus@gmail.com>
2023-05-23 01:55:10 +0300

    add documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""script return TO-DO list for employee given Employee ID"""
 import requests
 import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    toDos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [t.get("title") for t in toDos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
+        user.get("name"), len(completed), len(toDos)))
     [print("\t {}".format(c)) for c in completed]
