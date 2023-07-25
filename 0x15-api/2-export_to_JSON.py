7f475328ffab878875b9621f94ad09c13764241a
klaus <oriokiklaus@gmail.com>
2023-07-25 05:52:23 +0300

    script extracts information based off user id and returns in json format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    to_dos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": username
+        } for todo in to_dos]}, jsonfile)
