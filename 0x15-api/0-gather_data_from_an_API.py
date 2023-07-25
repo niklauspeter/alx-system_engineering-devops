6545cdd2c56662286b58a7803febdb5d2b08d32b
klaus <oriokiklaus@gmail.com>
2023-07-25 05:33:22 +0300

    script takes in employee id as input and returns to do list information

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script returns some to-do list information for a employee ID provided."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    to_do_s = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in to_do_s if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(to_do_s)))
+    [print("\t {}".format(c)) for c in completed]
