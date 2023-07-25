c07282bc4c04c3e403cc99b561bac997aec6f6a9
klaus <oriokiklaus@gmail.com>
2023-07-25 05:36:43 +0300

    reduce character count

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,9 +6,9 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    to_do_s = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    to_dos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in to_do_s if t.get("completed") is True]
+    completed = [t.get("title") for t in to_dos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(to_do_s)))
+        user.get("name"), len(completed), len(to_dos)))
     [print("\t {}".format(c)) for c in completed]
