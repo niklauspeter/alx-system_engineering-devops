861eea03ae6c106f8c73cb9dcc7f8eaacf3fa37a
klaus <oriokiklaus@gmail.com>
2023-07-25 05:42:26 +0300

    script exports extracted employee data to CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script exports to-do list information for an  employee ID using CSV format."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    user_name = user.get("username")
+    to_dos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, user_name, t.get("completed"), t.get("title")]
+        ) for t in to_dos]
