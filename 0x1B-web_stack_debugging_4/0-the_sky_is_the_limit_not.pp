59e0ce104039b07add019c7e6c020bfe402eaca5
klaus <oriokiklaus@gmail.com>
2023-08-14 23:33:25 +0300

    create puppet script to increase traffic nginx server can handle

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Puppet script Increases amount of traffic that Nginx server can handle
+
+# Start by increasing the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+# Then restart Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
